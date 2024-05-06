d42f0d3a1ff30b14f1d2ef33ba9107bd50476dad
Артур Хамидуллин <artrimskiy@gmail.com>
2024-05-06 19:06:39 +0500

    correct DATABASES

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courswork_6_db',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'PASSWORD': '123'
     }
 }
 
